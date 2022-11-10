0db31f073815df6384969c55af9c8df499b5bb84
Sverre Boschman <1142569+sboschman@users.noreply.github.com>
2022-11-10 13:49:49 +0100

    fix: add rbac to generation

--- controllers/keyhubsecret_controller.go
+++ controllers/keyhubsecret_controller.go
@@ -45,6 +45,9 @@ type KeyHubSecretReconciler struct {
 // +kubebuilder:rbac:groups=keyhub.topicus.nl,resources=keyhubsecrets/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=keyhub.topicus.nl,resources=keyhubsecrets/finalizers,verbs=update
 
+// +kubebuilder:rbac:groups=,resources=namespaces,verbs=get;list;watch
+// +kubebuilder:rbac:groups=,resources=secrets,verbs=get;list;watch;create;update;patch;delete
+
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
 // TODO(user): Modify the Reconcile function to compare the state specified by
